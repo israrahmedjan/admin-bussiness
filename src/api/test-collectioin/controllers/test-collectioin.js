6d16f70baa3cc3926c6dadac0a5dfe6c526e27cf
israrahmedjan <123giIsrar456>
2025-04-23 23:20:54 +0500

    some new data is added

--- src/api/test-collectioin/controllers/test-collectioin.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test-collectioin controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::test-collectioin.test-collectioin');
--- src/api/test-collectioin/routes/test-collectioin.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test-collectioin router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::test-collectioin.test-collectioin');
